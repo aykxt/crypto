82b58564fbb5112d74cce882d44172cb5cb7e02b
Aykut Yilmaz <aykuxt@gmail.com>
2020-10-13 13:24:56 +0200

    tests: Use assertEquals for comparison

--- tests/blowfish/blowfish.test.ts
+++ tests/blowfish/blowfish.test.ts
@@ -1,4 +1,4 @@
-import { assert } from "https://deno.land/std@0.74.0/testing/asserts.ts";
+import { assertEquals } from "https://deno.land/std@0.74.0/testing/asserts.ts";
 
 import Blowfish from "../../src/blowfish/mod.ts";
 
@@ -6,12 +6,13 @@ Deno.test("BF-ECB", () => {
   const bf = new Blowfish("abcdefgh");
 
   const original = new Uint8Array([97, 98, 99, 100, 101, 102, 103, 104]);
-  const enc = bf.encode(original);
-  const dec = bf.decode(enc);
+  const expectedEnc = new Uint8Array([49, 76, 81, 95, 230, 247, 169, 237]);
 
-  original.forEach((value, idx) => {
-    assert(dec[idx] === value);
-  });
+  const enc = bf.encode(original);
+  assertEquals(enc, expectedEnc);
+
+  const dec = bf.decode(enc);
+  assertEquals(dec, original);
 });
 
 Deno.test("BF-CBC", () => {
@@ -24,12 +25,8 @@ Deno.test("BF-CBC", () => {
   const expectedEnc = new Uint8Array([58, 232, 212, 65, 248, 24, 140, 110]);
 
   const enc = bf.encode(original);
-  enc.forEach((value, idx) => {
-    assert(expectedEnc[idx] === value);
-  });
+  assertEquals(enc, expectedEnc);
 
   const dec = bf.decode(enc);
-  original.forEach((value, idx) => {
-    assert(dec[idx] === value);
-  });
+  assertEquals(dec, original);
 });
