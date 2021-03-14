d74ae7434533dd50c96515e6606c7e04512fa722
Aykut Yilmaz <aykuxt@gmail.com>
2021-03-14 23:17:44 +0100

    refactor!: remove root mod.ts

--- hmac.ts
+++ hmac.ts
@@ -1,1 +1,2 @@
 export { hmac } from "./src/hmac/mod.ts";
+export type { SupportedAlgorithm } from "./src/hmac/mod.ts";
--- mod.ts
+++
@@ -1,8 +0,0 @@
-export {
-  BlowfishCbc,
-  BlowfishCfb,
-  BlowfishEcb,
-  BlowfishOfb,
-} from "./blowfish.ts";
-export { AesCbc, AesCfb, AesEcb, AesOfb } from "./aes.ts";
-export { Padding } from "./src/utils/padding.ts";
